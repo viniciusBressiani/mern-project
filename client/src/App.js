a0d68b841b7b87f1cb02bfe66b57bb550b9b5d92
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-22 10:09:17 -0300

    Daily & Monthly back and front

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,9 @@ import Products from "scenes/products"
 import Customers from 'scenes/customers';
 import Transactions from 'scenes/transactions';
 import  Geography  from 'scenes/geography';
-import Overview from "scenes/Overview";
+import Overview from "scenes/overview";
+import Daily from 'scenes/daily';
+import Monthly from 'scenes/monthly';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -31,6 +33,8 @@ function App() {
               <Route path="/transactions" element={<Transactions/>} />
               <Route path="/geography" element={<Geography/>} />
               <Route path="/overview" element={<Overview/>} />
+              <Route path="/daily" element={<Daily/>} />
+              <Route path="/monthly" element={<Monthly/>} />
             </Route>
           </Routes>
 
--- server/controllers/sales.js
+++ server/controllers/sales.js
@@ -7,6 +7,6 @@ export const getSales = async(req, res) => {
         res.status(200).json(overallStats[0]);
     } catch (error) {
         res.status(404).json({message: error.message});
-    }
+    } 
 
 }
